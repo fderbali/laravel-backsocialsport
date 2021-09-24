cef66d1fb903a736c73c9c3a789a9bf37a8b8c1f
Fahmi Derbali <fahmiderbali@gmail.com>
2021-09-23 21:46:29 -0400

    fix dans les routes

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResources([
-    'photos' => ActiviteController::class,
+    'activite' => ActiviteController::class,
 ]);
